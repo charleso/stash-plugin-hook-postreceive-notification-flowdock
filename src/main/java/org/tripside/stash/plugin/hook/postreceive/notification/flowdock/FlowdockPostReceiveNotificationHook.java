3489b76849a45eb3d966f07b5f7cb58e0b964437
Mike Eldridge <diz@cpan.org>
2013-04-09 21:00:50 -0500

    update the hook to use Flowdock's git API

--- src/main/java/org/tripside/stash/plugin/hook/postreceive/notification/flowdock/FlowdockPostReceiveNotificationHook.java
+++ src/main/java/org/tripside/stash/plugin/hook/postreceive/notification/flowdock/FlowdockPostReceiveNotificationHook.java
@@ -1,214 +1,195 @@
 package org.tripside.stash.plugin.hook.postreceive.notification.flowdock;
 
-import com.atlassian.stash.hook.repository.*;
-import com.atlassian.stash.repository.*;
-import com.atlassian.stash.setting.*;
-
-import com.atlassian.soy.renderer.SoyException;
-import com.atlassian.soy.renderer.SoyTemplateRenderer;
-
-import com.atlassian.stash.history.HistoryService;
+import com.atlassian.stash.content.MinimalChangeset;
 import com.atlassian.stash.content.Changeset;
-
+import com.atlassian.stash.content.Change;
+import com.atlassian.stash.content.ChangeType;
+import com.atlassian.stash.history.HistoryService;
+import com.atlassian.stash.hook.repository.AsyncPostReceiveRepositoryHook;
+import com.atlassian.stash.hook.repository.RepositoryHookContext;
 import com.atlassian.stash.nav.NavBuilder;
+import com.atlassian.stash.repository.Repository;
+import com.atlassian.stash.repository.RefChange;
+import com.atlassian.stash.repository.RefChangeType;
 import com.atlassian.stash.scm.git.GitRefPattern;
-import com.atlassian.stash.server.ApplicationPropertiesService;
-import com.atlassian.stash.user.StashUser;
-import com.atlassian.stash.user.StashAuthenticationContext;
+import com.atlassian.stash.setting.RepositorySettingsValidator;
+import com.atlassian.stash.setting.SettingsValidationErrors;
+import com.atlassian.stash.setting.Settings;
 import com.atlassian.stash.util.Page;
+import com.atlassian.stash.util.PageRequest;
 import com.atlassian.stash.util.PageRequestImpl;
 
 import java.net.URI;
+import java.text.SimpleDateFormat;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.ArrayList;
 
-import us.monoid.web.Resty;
-import us.monoid.web.JSONResource;
 import us.monoid.json.JSONObject;
+import us.monoid.web.TextResource;
+import us.monoid.web.Resty;
 
 import static us.monoid.web.Resty.content;
+import static us.monoid.web.Resty.data;
+import static us.monoid.web.Resty.form;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.google.common.base.Predicate;
-import com.google.common.collect.Iterables;
-
-import static com.google.common.collect.Collections2.filter;
-
-import org.apache.commons.lang.StringUtils;
-
 public class FlowdockPostReceiveNotificationHook implements AsyncPostReceiveRepositoryHook, RepositorySettingsValidator {
 	private static final Logger log = LoggerFactory.getLogger(FlowdockPostReceiveNotificationHook.class);
 
-	private final ApplicationPropertiesService applicationPropertiesService;
-	private final StashAuthenticationContext authenticationContext;
-	private final HistoryService historyService;
-	private final NavBuilder navBuilder;
-	private final SoyTemplateRenderer soyTemplateRenderer;
+	private final SimpleDateFormat	iso8601;
+	private final HistoryService	historyService;
+	private final NavBuilder		navBuilder;
 
-	private static final Predicate<RefChange> IS_BRANCH = new Predicate<RefChange>()
+	public FlowdockPostReceiveNotificationHook (HistoryService historyService, NavBuilder navBuilder)
 	{
-		@Override
-		public boolean apply(RefChange input) {
-			return input.getRefId().startsWith(GitRefPattern.HEADS.getPath());
-		}
-	};
-
-	public FlowdockPostReceiveNotificationHook (ApplicationPropertiesService applicationPropertiesService, StashAuthenticationContext authenticationContext, HistoryService historyService, NavBuilder navBuilder, SoyTemplateRenderer soyTemplateRenderer)
-	{
-		this.applicationPropertiesService = applicationPropertiesService;
-		this.authenticationContext = authenticationContext;
-		this.historyService = historyService;
-		this.navBuilder = navBuilder;
-		this.soyTemplateRenderer = soyTemplateRenderer;
+		this.iso8601		= new SimpleDateFormat ("yyyy-MM-dd'T'HH:mm:ssZ");
+		this.historyService	= historyService;
+		this.navBuilder		= navBuilder;
 	}
 
 	@Override
-	public void postReceive(RepositoryHookContext context, Collection<RefChange> refChanges)
+	public void postReceive (RepositoryHookContext context, Collection <RefChange> refChanges)
 	{
 		try {
 			processRefChanges(context, refChanges);
 		} catch (Exception e) {
-			log.error(e.toString());
+			log.error("FAIL", e);
 		}
 	}
 
-	private void processRefChanges(RepositoryHookContext context, Collection<RefChange> refChanges) throws Exception
+	private void processRefChanges (RepositoryHookContext context, Collection <RefChange> refChanges) throws Exception
 	{
 		String token = context.getSettings().getString("token");
-		//String email = context.getSettings().getString("email");
 
-		//if (email == null || email.isEmpty())
-		//	email = applicationPropertiesService.getServerEmailAddress();
-
-		//if (email == null || email.isEmpty())
-		//	throw new Exception("OMG NO EMAIL");
 		if (token == null || token.isEmpty())
 			throw new Exception("OMG NO TOKEN");
 
-		Repository repo = context.getRepository();
-
-		for (RefChange rc : refChanges) {
-			String ref		= rc.getRefId();
-
-			Page<Changeset> pageCommits = historyService.getChangesetsBetween(repo, rc.getFromHash(), rc.getToHash(), new PageRequestImpl(0, 5));
-			GitRefPattern refPattern = null;
-
-			StringBuilder soyTemplateName	= new StringBuilder ("inbox");
-			StringBuilder subject			= new StringBuilder ("");
-
-			switch (rc.getType()) {
-				case ADD:		subject.append("created "); break;
-				case DELETE:	subject.append("deleted "); break;
-				case UPDATE:	subject.append("updated "); break;
-			}
-
-			if (ref.startsWith(GitRefPattern.HEADS.getPath())) {
-				refPattern = GitRefPattern.HEADS;
-
-				soyTemplateName.append("Branch");
-				subject.append("branch ");
-			}
-
-			if (ref.startsWith(GitRefPattern.TAGS.getPath())) {
-				refPattern = GitRefPattern.TAGS;
-
-				soyTemplateName.append("Tag");
-				subject.append("tag ");
-			}
-
-			if (refPattern == null)
-				throw new Exception("RefChange was neither a HEAD nor a TAG; confused");
-
-			subject.append(refPattern.unqualify(ref));
-			soyTemplateName.append(rc.getType().name().substring(0,1) + rc.getType().name().toLowerCase().substring(1));
-
-			postNotification(repo, token, subject.toString(), buildNotificationContent(repo, refPattern.unqualify(ref), pageCommits, soyTemplateName.toString()));
-
-			/*if (type.equals("ADD")) {
-				// ref was created
-				postRefAddNotification(email, token, ref, commits);
-			} else if (type.equals("DELETE") {
-				// ref was deleted
-				postRefDeleteNotification(email, token, ref, commits);
-			} else if (type.equals("UPDATE") {
-				// ref was updated
-				postRefUpdateNotification(email, token, ref, commits);
-			} else {
-				throw new Exception("unknown RefChange type '" + type + "'");
-			}*/
-		}
+		for (RefChange rc : refChanges)
+			postNotification(token, buildPayloadForRefChange(context.getRepository(), rc));
 	}
 
-	private void postNotification(Repository repo, String token, String subject, String content) throws Exception
+	private HashMap <String, Object> buildPayloadForRefChange (Repository repo, RefChange rc) throws Exception
 	{
-		URI uri = new URI("https://api.flowdock.com/v1/messages/team_inbox/" + token);
+		HashMap <String, Object>	payload		= new HashMap ();
+		HashMap <String, Object>	repository	= new HashMap ();
+		ArrayList <Object>			commits		= new ArrayList ();
+
+		String ref = rc.getRefId();
+		GitRefPattern refPattern = null;
+
+		if (ref.startsWith(GitRefPattern.HEADS.getPath()))
+			refPattern = GitRefPattern.HEADS;
+		if (ref.startsWith(GitRefPattern.TAGS.getPath()))
+			refPattern = GitRefPattern.TAGS;
+
+		if (refPattern == null)
+			throw new Exception("ref was neither a head nor a tag; I am so confused");
+
+		repository.put("url", navBuilder.repo(repo).buildConfigured());
+		repository.put("name", repo.getProject().getName() + " - " + repo.getName());
+
+		PageRequest pageRequest = (PageRequest) new PageRequestImpl (0, 32);
+
+		if (rc.getType() == RefChangeType.UPDATE) {
+			while (pageRequest != null) {
+				Page <Changeset> pageChangesets = historyService.getChangesetsBetween(repo, rc.getFromHash(), rc.getToHash(), pageRequest);
+
+				for (Changeset changeset : pageChangesets.getValues())
+					commits.add(buildPayloadForCommit(repo, changeset));
+
+				pageRequest = pageChangesets.getNextPageRequest();
+			}
+		}
+
+		payload.put("before", rc.getFromHash());
+		payload.put("after", rc.getToHash());
+		payload.put("repository", repository);
+		payload.put("commits", commits);
+		payload.put("ref", ref);
+		payload.put("ref_name", refPattern.unqualify(ref));
+
+		return payload;
+	}
+
+	private HashMap <String, Object> buildPayloadForCommit (Repository repo, Changeset changeset) throws Exception
+	{
+		HashMap <String, Object>	commit		= new HashMap ();
+		HashMap <String, Object>	author		= new HashMap ();
+		ArrayList <String>			added		= new ArrayList ();
+		ArrayList <String>			modified	= new ArrayList ();
+		ArrayList <String>			removed		= new ArrayList ();
+
+		Collection <MinimalChangeset> parents = changeset.getParents();
+
+		if (parents.size() > 1)
+			throw new Exception("it's a merge commit or something; I am so confused!");
+
+		PageRequest pageRequest = (PageRequest) new PageRequestImpl (0, 32);
+
+		while (pageRequest != null) {
+			Page <Change> pageChanges = historyService.getChanges(repo, changeset.getId(), parents.iterator().next().getId(), pageRequest);
+
+			for (Change change : pageChanges.getValues()) {
+				switch (change.getType()) {
+					case ADD:
+					case COPY:
+						added.add(change.getPath().toString());
+						break;
+
+					case MOVE:
+						removed.add(change.getSrcPath().toString());
+						added.add(change.getPath().toString());
+						break;
+
+					case MODIFY: modified.add(change.getPath().toString()); break;
+					case DELETE: removed.add(change.getPath().toString()); break;
+
+					case UNKNOWN:
+						throw new Exception("change type is unknown; I am so confused!");
+				}
+			}
+
+			pageRequest = pageChanges.getNextPageRequest();
+		}
+
+		author.put("email", changeset.getAuthor().getEmailAddress());
+		author.put("name", changeset.getAuthor().getName());
+
+		commit.put("id", changeset.getId());
+		commit.put("timestamp", iso8601.format(changeset.getAuthorTimestamp()));
+		commit.put("url", navBuilder.repo(repo).changeset(changeset.getId()).buildConfigured());
+		commit.put("author", author);
+		commit.put("message", changeset.getMessage());
+		commit.put("added", added);
+		commit.put("modified", modified);
+		commit.put("removed", removed);
+
+		return commit;
+	}
+
+	private void postNotification (String token, HashMap <String, Object> payload) throws Exception
+	{
+		URI uri = new URI("https://api.flowdock.com/v1/git/" + token);
 		Resty r = new Resty();
-		JSONObject json = new JSONObject();
 
-		StashUser user = authenticationContext.getCurrentUser();
+		log.warn(new JSONObject (payload).toString(4));
 
-		json.put("source", repo.getName());
-		json.put("project", repo.getProject().getName());
-		json.put("from_address", user.getEmailAddress());
-		json.put("link", navBuilder.repo(repo).buildConfigured());
-		json.put("subject", subject);
-		json.put("content", content);
+		TextResource res = r.text(uri, form(data("payload", content(new JSONObject (payload)))));
 
-		log.info("URI: " + uri);
-		log.info("Request JSON: " + json.toString());
-
-		JSONResource res = r.json(uri, content(json));
-
-		log.info("Response: " + res.toObject().toString());
+		log.info("Response: " + res.toString());
 	}
-
-	private String buildNotificationContent(Repository repo, String refName, Page<Changeset> pageCommits, String soyTemplateName) throws SoyException
-	{
-		Collection changesets = new ArrayList<HashMap<String, Object>> ();
-
-		for (Changeset changeset : Iterables.limit(pageCommits.getValues(), 3)) {
-			HashMap<String, Object> csdata = new HashMap();
-
-			csdata.put("message", changeset.getMessage());
-			csdata.put("sha1", changeset.getId().substring(0, 7));
-			csdata.put("uri", navBuilder.repo(repo).changeset(changeset.getId()).buildConfigured());
-
-			changesets.add(csdata);
-		}
-
-		HashMap<String, Object> params = new HashMap();
-
-		StashUser user = authenticationContext.getCurrentUser();
-
-		params.put("user", authenticationContext.getCurrentUser());
-		params.put("uri", navBuilder.repo(repo).buildConfigured());
-		params.put("project", repo.getProject().getName());
-		params.put("repo", repo.getName());
-		params.put("refName", refName);
-		params.put("changesets", changesets);
-
-		return soyTemplateRenderer.render("org.tripside.stash.plugin.hook.postreceive.notification.flowdock:templates", "org.tripside.stash.plugin.hook.postreceive.notification.flowdock." + soyTemplateName, params);
-	}
-
 
 	@Override
-	public void validate(Settings settings, SettingsValidationErrors errors, Repository repository)
+	public void validate (Settings settings, SettingsValidationErrors errors, Repository repository)
 	{
 		String token = settings.getString("token", "");
-		//String email = settings.getString("email", "");
-
-		//if (email.isEmpty())
-		//	email = applicationPropertiesService.getServerEmailAddress();
 
 		if (token.isEmpty())
 			errors.addFieldError("token", "A valid Flowdock API token is required");
 		else if (!token.matches("^[a-f0-9]{32}$"))
 			errors.addFieldError("token", "A valid 32 digit hexadecimal Flowdock API token is required");
-
-		//if (email == null || email.isEmpty())
-		//	errors.addFieldError("email", "Server email address is empty; an email address must be provided");
 	}
 }
